f00df975ce74bb75d798c89c1d67d8846f91128f
de-gupta <de-gupta@apollo>
2026-04-08 17:50:06 +0200

    added deps

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module de.gupta.security.argus
+{
+	requires de.gupta.security.hermes;
+	requires de.gupta.security.augustus;
+	requires de.gupta.security.themis;
+
+	requires de.gupta.aletheia;
+	requires de.gupta.athena;
+}
